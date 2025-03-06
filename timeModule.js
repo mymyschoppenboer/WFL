a4e1fefa657540b06c0d4e79dda9450ba2dcd54a
Mymy Schoppenboer <ongezelligproject@gmail.com>
2025-03-06 09:25:27 -0700

    Delete timeModule.js

--- timeModule.js
+++
@@ -1,9 +0,0 @@
-export function updateTime() {
-    const timeElement = document.querySelector('.time');
-    const now = new Date();
-    const timeString = now.toLocaleTimeString([], { 
-      hour: '2-digit', 
-      minute: '2-digit'
-    });
-    timeElement.textContent = timeString;
-  }
